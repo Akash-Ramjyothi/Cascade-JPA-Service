98208edf2f5172f279010561615195e61d3f4756
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-12-07 23:48:50 +0530

    Added CLR Lambda function

--- src/main/java/com/jpa/mappings/MappingsApplication.java
+++ src/main/java/com/jpa/mappings/MappingsApplication.java
@@ -1,13 +1,21 @@
 package com.jpa.mappings;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MappingsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MappingsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MappingsApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> {
+            System.out.println("Hello JPA");
+        };
+    }
 }
